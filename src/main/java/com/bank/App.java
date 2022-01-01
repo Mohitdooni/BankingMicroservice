2e085ceabd4926a76449660b3628a0e7a0355423
raman <raman@gmail.com>
2022-01-01 17:31:53 +0000

    merge code

--- src/main/java/com/bank/App.java
+++ src/main/java/com/bank/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello DevOps-done by dev1" );
+        System.out.println( "Hello DevOps- done by dev2" );
+>>>>>>> dev2
     }
 }
